712e618f12446348e962fff5a83d25b9bfa52d40
Edwin Cacuango <75394193+EdwinCacuango@users.noreply.github.com>
2022-01-06 23:20:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,5 @@ console.log(authorSection);
 console.log(shareBtn);
 
 shareBtn.addEventListener("click", () => {
-  if (authorSection.classList.contains("active")) {
-    authorSection.classList.remove("active");
-    authorSection.style.background="none"
-  } else {
-    authorSection.style.backgroundColor = "hsl(217, 19%, 35%)";
-    authorSection.classList.add("active");
-  }
+  authorSection.classList.toggle("active") 
 });
