2c084ecb9788d2f3518f3ab8f77a05ac0cb3895b
Edwin Cacuango <edwin-fernando@hotmail.es>
2022-01-08 19:02:28 -0500

    Added documentation

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 let authorSection = document.getElementById("author-section");
 let shareBtn = document.getElementById("share-btn");
 
-console.log(authorSection);
-console.log(shareBtn);
-
 shareBtn.addEventListener("click", () => {
   authorSection.classList.toggle("active") 
 });
--- note.md
+++
@@ -1,3 +0,0 @@
-links de referencia
-https://www.youtube.com/watch?v=y2FDDzzl2X8
-https://www.youtube.com/watch?v=wh2yuslnGG4
